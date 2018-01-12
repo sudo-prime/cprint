3b59c0a7f74d3b13fded88643f1cf451777820e3
surrsurus <surrsurus@protonmail.com>
2018-01-12 15:14:41 -0500

    Add cin

---
+++ test.py
@@ -0,0 +1,12 @@
+from cprint import *
+
+cout << "Hello world" << '\n';
+
+cout << "endl works" << endl;
+
+cout << "What has science done? " << cin << endl;
+
+# Needs work
+a = "huh"
+a = (cin >> a)
+print(a)
